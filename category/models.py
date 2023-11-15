cc60b7b99c6a5eb29161c17a5b3a736765b4c25f
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-11-16 00:21:06 +0530

    Category Model Setup

---
+++ category/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50,unique=True)
+    slug = models.SlugField(null=False,unique=True)
+    description = models.TextField( max_length=255,blank=True )
+    category_image = models.ImageField(upload_to='images/categories',blank=True)
+
+    def __str__(self) -> str:
+        return self.category_name
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
