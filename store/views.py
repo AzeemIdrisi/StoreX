c023297bfa5d7c60d656ba2e3338b8e3a3267bc9
Azeem Idrisi <azeemidrisi@protonmail.com>
2023-11-21 01:19:45 +0530

    Added cart counter, added to cart

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from cart.models import CartItem
+from cart.views import _cart_id
 
 
 # Create your views here.
@@ -27,10 +29,14 @@ def product_detail(request, category_slug, product_slug):
         category__slug=category_slug,  # Double underscore is used to access nested attribute
         slug=product_slug,
     )
+    in_cart = CartItem.objects.filter(
+        cart__cart_id=_cart_id(request), product=product
+    ).exists()
     return render(
         request,
         "store/product_detail.html",
         {
             "product": product,
+            "in_cart": in_cart,
         },
     )
